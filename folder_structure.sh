9b7dca124e68f6c9d9db5295e9b29e3a884d1c37
Shivam prasad <shivam13juna@gmail.com>
2025-02-01 15:06:51 +0530

    Adding more structure to src, and dummy tests

---
+++ folder_structure.sh
@@ -0,0 +1,8 @@
+mkdir -p project/src/{data,models,training,evaluation,inference,utils}
+touch project/src/{__init__.py,config.py}
+touch project/src/data/{__init__.py,data_loader.py}
+touch project/src/models/{__init__.py,model.py}
+touch project/src/training/{__init__.py,trainer.py}
+touch project/src/evaluation/{__init__.py,evaluator.py}
+touch project/src/inference/{__init__.py,predict.py}
+touch project/src/utils/{__init__.py,metrics.py}
---
+++ tests/test_train.py
@@ -0,0 +1,16 @@
+# import libraries
+import sys
+import pyrootutils
+root = pyrootutils.setup_root(sys.path[0], pythonpath=True, cwd=True)
+
+from src.main import add_two_numbers
+import pytest
+
+def test_add_two_numbers():
+	assert add_two_numbers(1, 2) == 3
+	assert add_two_numbers(0, 0) == 0
+	assert add_two_numbers(-1, 1) == 0
+	assert add_two_numbers(-1, -1) == -2
+	assert add_two_numbers(1, -1) == 0
+
+	
